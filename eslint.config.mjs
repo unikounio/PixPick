8b3a71f086c86e5f2ff328c978bac34e4f570fda
unikounio <bubbbble428@gmail.com>
2024-09-27 17:28:13 +0900

    eslintを設定した

---
+++ eslint.config.mjs
@@ -0,0 +1,18 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import eslintConfigPrettier from "eslint-config-prettier";
+
+export default [
+  {
+    languageOptions: {
+      ecmaVersion: "latest",
+      sourceType: "module",
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
+    },
+  },
+  pluginJs.configs.recommended,
+  eslintConfigPrettier,
+];
