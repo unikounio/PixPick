02557bed4869726c3d754f498ec479e6ceea0477
unikounio <bubbbble428@gmail.com>
2024-10-03 12:03:52 +0900

    Userモデルを編集

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,11 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+  devise :omniauthable, omniauth_providers: %i[google_oauth2]
+  validates :uid, uniqueness: { scope: :provider }
+  def self.from_omniauth(auth)
+    user = find_or_create_by(provider: auth.provider, uid: auth.uid)
+    user.name = auth.info.name
+    user.avatar_url = auth.info.image
+    user.save!
+    user
+  end
 end
