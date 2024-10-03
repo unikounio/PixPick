bf5964bfb04e9c29ec2e0099ebd5abfdaf199638
unikounio <bubbbble428@gmail.com>
2024-10-03 21:12:25 +0900

    ContestsとHomeコントローラーを作成し、ログイン機能を実装

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,10 @@
+class HomeController < ApplicationController
+  skip_before_action :authenticate_user!, only: [ :index ]
+  def index
+    if user_signed_in?
+      redirect_to new_contest_path
+    else
+      render :welcome
+    end
+  end
+end
