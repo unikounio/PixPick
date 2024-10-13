c75620cb3170bda956dee160aa6975d5cde9fb2a
unikounio <bubbbble428@gmail.com>
2024-10-13 22:37:04 +0900

    OAuthトークン管理に関するメソッドとそのテストを追加

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -3,9 +3,13 @@
 module Users
   class OmniauthCallbacksController < Devise::OmniauthCallbacksController
     def google_oauth2
-      @user = User.from_omniauth(request.env['omniauth.auth'])
+      auth = request.env['omniauth.auth']
+      @user = User.from_omniauth(auth)
+
       if @user.persisted?
         request.env['devise.mapping'] = Devise.mappings[:user]
+        store_tokens(auth.credentials)
+
         sign_in_and_redirect @user, event: :authentication
         set_flash_message(:notice, :success, kind: 'Google') if is_navigational_format?
       else
@@ -16,9 +20,17 @@ module Users
     def failure
       error = request.env['omniauth.error']
       error_type = request.env['omniauth.error.type']
-      Rails.logger.error "OmniAuth認証失敗: #{error.message} (Type: #{error_type})"
+      logger.error "OmniAuth認証失敗: #{error.message} (Type: #{error_type})"
 
       redirect_to root_path, alert: '認証に失敗しました。もう一度お試しください。'
+    end
+
+    private
+
+    def store_tokens(credentials)
+      session[:access_token] = credentials.token
+      session[:refresh_token] = credentials.refresh_token
+      session[:token_expires_at] = credentials.expires_at
     end
   end
 end
