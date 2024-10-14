191a8f3068482dd8be660be61ad97ba5d687ba0a
unikounio <bubbbble428@gmail.com>
2024-10-14 17:21:54 +0900

    Contestsに関するルーティングを設定

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,14 @@
 
 Rails.application.routes.draw do
   root 'home#index'
+
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
   devise_scope :user do
     delete 'logout', to: 'devise/sessions#destroy', as: :destroy_user_session
   end
-  resources :contests
+
+  resources :contests, except: :index
+  get 'users/:user_id/contests', to: 'contests#index', as: :user_contests
 
   get 'photos/index', to: 'photos#index'
 
