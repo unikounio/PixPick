b4ccc9c2dc46e3a570270ecf909413f262e14d42
unikounio <bubbbble428@gmail.com>
2024-10-16 10:34:35 +0900

    コンテスト作成機能を実装

--- app/controllers/contests_controller.rb
+++ app/controllers/contests_controller.rb
@@ -1,5 +1,30 @@
 # frozen_string_literal: true
 
 class ContestsController < ApplicationController
-  def new; end
+  def index
+    @contests = current_user.contests
+    render :index
+  end
+
+  def new
+    @contest = Contest.new
+    @contests = current_user.contests
+  end
+
+  def create
+    ActiveRecord::Base.transaction do
+      @contest = current_user.contests.new(contest_params)
+      @contest.save!
+      @contest.participants.create!(user: current_user)
+      redirect_to new_contest_entry_path(@contest), notice: 'コンテストを作成しました。'
+    end
+  rescue ActiveRecord::RecordInvalid
+    render :new, alert: 'コンテストの作成に失敗しました。'
+  end
+end
+
+  private
+
+def contest_params
+  params.require(:contest).permit(:name, :deadline)
 end
---
+++ app/controllers/entries_controller.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+class EntriesController < ApplicationController
+  def new; end
+end
