b17cda6ba5b6bef0966d5a4a3449a7cf22120ce6
unikounio <bubbbble428@gmail.com>
2024-10-17 20:58:17 +0900

    コンテスト作成機能に関するモデルテストを追加

--- spec/factories/contests.rb
+++ spec/factories/contests.rb
@@ -2,7 +2,7 @@
 
 FactoryBot.define do
   factory :contest do
-    name { 'MyString' }
-    deadline { '2024-10-14 14:44:41' }
+    name { 'Test Contest' }
+    deadline { Time.zone.now.beginning_of_day }
   end
 end
--- spec/models/contest_spec.rb
+++ spec/models/contest_spec.rb
@@ -3,5 +3,30 @@
 require 'rails_helper'
 
 RSpec.describe Contest do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'Validations' do
+    let(:contest) { described_class.new(name: 'Photo Contest', deadline:) }
+    let(:deadline) { Time.zone.today + 1.day }
+
+    it 'is valid with valid attributes' do
+      expect(contest).to be_valid
+    end
+
+    it 'is invalid without a name' do
+      contest.name = nil
+      expect(contest).not_to be_valid
+      expect(contest.errors[:name]).to include(I18n.t('errors.messages.blank'))
+    end
+
+    it 'is invalid without a deadline' do
+      contest.deadline = nil
+      expect(contest).not_to be_valid
+      expect(contest.errors[:deadline]).to include(I18n.t('errors.messages.blank'))
+    end
+
+    it 'is invalid with a past deadline' do
+      contest.deadline = Time.zone.today - 1.day
+      expect(contest).not_to be_valid
+      expect(contest.errors[:deadline]).to include(I18n.t('activerecord.errors.messages.past', attribute: I18n.t('activerecord.attributes.contest.deadline')))
+    end
+  end
 end
