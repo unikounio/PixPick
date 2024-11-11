587fbfb0ea593e6e949a1e5181954a0201b486df
unikounio <bubbbble428@gmail.com>
2024-11-11 21:25:16 +0900

    ヘッダーにドロップダウンメニューを追加

--- config/importmap.rb
+++ config/importmap.rb
@@ -7,4 +7,4 @@ pin '@hotwired/turbo-rails', to: 'turbo.min.js'
 pin '@hotwired/stimulus', to: 'stimulus.min.js'
 pin '@hotwired/stimulus-loading', to: 'stimulus-loading.js'
 pin_all_from 'app/javascript/controllers', under: 'controllers'
-pin "tailwindcss-stimulus-components" # @6.1.2
+pin 'tailwindcss-stimulus-components' # @6.1.2
