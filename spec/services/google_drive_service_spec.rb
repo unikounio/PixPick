597299d6d5c79286620ba33f78b4d8d66e7e4e08
unikounio <bubbbble428@gmail.com>
2024-11-19 16:59:19 +0900

    GoogleDriveService#upload_fileの単体テストを追加

--- spec/services/google_drive_service_spec.rb
+++ spec/services/google_drive_service_spec.rb
@@ -53,4 +53,51 @@ RSpec.describe GoogleDriveService do
       end
     end
   end
+
+  describe '#upload_file' do
+    let(:file) do
+      instance_double(
+        ActionDispatch::Http::UploadedFile,
+        original_filename: 'test_image.jpg',
+        path: '/path/to/mock/file',
+        content_type: 'image/jpeg'
+      )
+    end
+    let(:folder_id) { 'mock_folder_id' }
+
+    context 'when the file upload is successful' do
+      let(:mock_response) { instance_double(Google::Apis::DriveV3::File, id: 'mock_drive_file_id') }
+
+      before do
+        allow(client).to receive(:create_file).with(
+          { name: file.original_filename, parents: [folder_id] },
+          upload_source: file.path,
+          content_type: file.content_type
+        ).and_return(mock_response)
+      end
+
+      it 'returns the drive file ID' do
+        result = drive_service.upload_file(file, folder_id)
+        expect(result).to eq('mock_drive_file_id')
+      end
+    end
+
+    context 'when the file upload fails' do
+      before do
+        allow(client).to receive(:create_file).with(
+          { name: file.original_filename, parents: [folder_id] },
+          upload_source: file.path,
+          content_type: file.content_type
+        ).and_raise(Google::Apis::Error.new('Upload failed'))
+      end
+
+      it 'logs an error and returns nil' do
+        allow(Rails.logger).to receive(:error)
+        result = drive_service.upload_file(file, folder_id)
+
+        expect(result).to be_nil
+        expect(Rails.logger).to have_received(:error).with('Google Driveアップロードに失敗しました: Upload failed')
+      end
+    end
+  end
 end
