190031503f4a082ee46874f9ad15a35cf6220de6
unikounio <bubbbble428@gmail.com>
2024-11-20 21:25:45 +0900

    Entries#createのtransactionを調整

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -1,26 +1,25 @@
 # frozen_string_literal: true
 
 class EntriesController < ApplicationController
+  before_action :set_contest, only: %i[new create]
   before_action :ensure_valid_access_token!, only: %i[new]
 
   def new
-    @contest = Contest.find(params[:contest_id])
     @folder_id = @contest.drive_file_id
     @access_token = session[:access_token]
   end
 
   def create
     files = files_params[:files]
-    contest = Contest.find(params[:contest_id])
 
     if files.blank?
       render json: { error: 'ファイルが見つかりません' }, status: :unprocessable_entity
       return
     end
 
-    begin
-      upload_and_create_entries!(files, contest)
-      render json: { redirect_url: contest_path(contest) }, status: :ok
+    ActiveRecord::Base.transaction do
+      upload_and_create_entries!(files, @contest)
+      render json: { redirect_url: contest_path(@contest) }, status: :ok
     rescue StandardError => e
       Rails.logger.error("ファイルの処理注に次のエラーが発生: #{e.message}")
       render json: { error: 'ファイルの処理中にエラーが発生しました' }, status: :unprocessable_entity
@@ -33,18 +32,20 @@ class EntriesController < ApplicationController
     params.permit(files: [])
   end
 
-  def upload_and_create_entries!(files, contest)
-    ActiveRecord::Base.transaction do
-      files.each do |file|
-        drive_file_id, permission_id = upload_to_google_drive(file, contest)
+  def set_contest
+    @contest = Contest.find(params[:contest_id])
+  end
 
-        Entry.create!(
-          user: current_user,
-          contest: contest,
-          drive_file_id: drive_file_id,
-          drive_permission_id: permission_id
-        )
-      end
+  def upload_and_create_entries!(files, contest)
+    files.each do |file|
+      drive_file_id, permission_id = upload_to_google_drive(file, contest)
+
+      Entry.create!(
+        user: current_user,
+        contest: contest,
+        drive_file_id: drive_file_id,
+        drive_permission_id: permission_id
+      )
     end
   end
 
