06dc875f1a7485f89499f01826fdb69e73b42327
unikounio <bubbbble428@gmail.com>
2024-11-25 22:07:12 +0900

    選択中のコンテスト名の表示と直近のコンテストへリンクするドロップダウンリストを追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,8 @@ class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
   before_action :authenticate_user!
+  before_action :set_recent_contests
+  before_action :set_selected_contest
 
   private
 
@@ -22,5 +24,22 @@ class ApplicationController < ActionController::Base
   def store_access_token(result)
     session[:access_token] = result['access_token']
     session[:token_expires_at] = Time.current + result['expires_in'].seconds
+  end
+
+  def set_selected_contest
+    contest_id = params[:contest_id] || params[:id]
+    return unless user_signed_in? && contest_id.present?
+
+    @selected_contest_name = Rails.cache.fetch("contest_name_#{contest_id}", expires_in: 12.hours) do
+      Contest.find(contest_id)&.name
+    end
+  end
+
+  def set_recent_contests
+    @recent_contests = if user_signed_in?
+                         current_user.contests.order(updated_at: :desc).limit(4)
+                       else
+                         []
+                       end
   end
 end
