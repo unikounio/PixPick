2c3710b5f0af89f16ec35f1d4b0a69b394086054
unikounio <bubbbble428@gmail.com>
2024-12-09 15:59:35 +0900

    コンテストページのタブ用のヘルパーを追加

--- app/helpers/contests_helper.rb
+++ app/helpers/contests_helper.rb
@@ -1,4 +1,11 @@
 # frozen_string_literal: true
 
 module ContestsHelper
+  def tab_link_classes(current_path, target_path, extra_classes = '')
+    if current_path == target_path
+      "bg-white text-black #{extra_classes}"
+    else
+      "bg-gray-100 text-gray-500 #{extra_classes}"
+    end
+  end
 end
