71a6c483005da1737a94ff3c399953b10e2a8b52
unikounio <bubbbble428@gmail.com>
2024-12-23 17:00:13 +0900

    トランザクションのスコープ調整

--- app/models/contest.rb
+++ app/models/contest.rb
@@ -59,7 +59,7 @@ class Contest < ApplicationRecord
   end
 
   def save_drive_ids(folder_id, permission_id)
-    ActiveRecord::Base.transaction do
+    transaction do
       self.drive_file_id = folder_id
       self.drive_permission_id = permission_id
       save!
