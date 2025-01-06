08336af624bd1a2f6ba9f1fa54c3d34687f3c51a
unikounio <bubbbble428@gmail.com>
2025-01-06 15:04:40 +0900

    metaタグを設定

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,29 @@
 # frozen_string_literal: true
 
 module ApplicationHelper
+  def default_meta_tags
+    {
+      site: 'PixPick',
+      reverse: true,
+      charset: 'utf-8',
+      description: 'PixPickは、家族や友人と写真のコンテストを開催できるアプリです。',
+      keywords: 'PixPick, 写真, 写真整理, 写真選び, コンテスト, ベストショット, アルバム, アルバム共有',
+      og: {
+        title: :title,
+        type: 'website',
+        site_name: 'PixPick',
+        description: :description,
+        image: image_url('ogp.png'),
+        url: 'https://pixpick.jp',
+        local: 'ja-JP'
+      },
+      twitter: {
+        card: 'summary_large_image',
+        site: '@unikounio512'
+      }
+    }
+  end
+
   def user_avatar_url
     current_user&.avatar_url || asset_path('default_avatar.png')
   end
