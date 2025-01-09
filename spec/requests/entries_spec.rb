c9a6a7e11522ebb2476d1b83baf9cddcab0e2cc8
unikounio <bubbbble428@gmail.com>
2025-01-09 16:10:36 +0900

    Entries#destroyのリクエストスペックを追加

--- spec/requests/entries_spec.rb
+++ spec/requests/entries_spec.rb
@@ -22,4 +22,56 @@ RSpec.describe "Entries", type: :request do
       expect(response.body).to include(next_entry.id.to_s)
     end
   end
+
+  describe "GET /contests/:contest_id/entries/new" do
+    it "responds with 200 OK when rendering the new page" do
+      get new_contest_entry_path(contest)
+      expect(response).to have_http_status(200)
+    end
+  end
+
+  describe "POST /contests/:contest_id/entries" do
+    file = Rack::Test::UploadedFile.new(Rails.root.join("spec/files/sample.jpg"), "image/jpeg")
+
+    context "with valid parameters" do
+      it "creates a new entry and returns JSON" do
+        post contest_entries_path(contest), params: { files: [file] }
+        expect(response).to have_http_status(200)
+        expect(response.body).to include("redirect_url")
+      end
+    end
+
+    context "with invalid parameters" do
+      it "returns an error message" do
+        post contest_entries_path(contest), params: { files: [] }, as: :json
+        expect(response).to have_http_status(:unprocessable_entity)
+        expect(response.body).to include("ファイルが見つかりません")
+      end
+    end
+  end
+
+  describe "DELETE /entries/:id" do
+    it "deletes the entry and returns Turbo Stream response" do
+      entry
+      expect {
+        delete contest_entry_path(contest, entry)
+      }.to change(Entry, :count).by(-1)
+
+      expect(response).to have_http_status(200)
+      expect(response.body).to include("entry_#{entry.id}")
+    end
+  end
+
+  it "does not delete entry if unauthorized" do
+    other_user = create(:user)
+    other_entry = create(:entry, contest: contest, user: other_user)
+
+    expect {
+      delete contest_entry_path(contest, other_entry)
+    }.not_to change(Entry, :count)
+
+    expect(response).to have_http_status(200)
+    expect(response.body).to include(I18n.t('activerecord.errors.messages.unauthorized',
+                                            model: I18n.t('activerecord.models.entry')))
+  end
 end
