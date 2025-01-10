760494c45fa265ee6c6037a82ed0349b3efee9cb
unikounio <bubbbble428@gmail.com>
2025-01-10 19:24:42 +0900

    参加者が0になったらコンテストを削除する機能を追加

--- app/models/participant.rb
+++ app/models/participant.rb
@@ -5,4 +5,12 @@ class Participant < ApplicationRecord
   belongs_to :user
 
   validates :user_id, uniqueness: { scope: :contest_id, message: :participated }
+
+  after_destroy :destroy_contest_if_no_participants
+
+  private
+
+  def destroy_contest_if_no_participants
+    contest.destroy if contest.participants.count.zero?
+  end
 end
