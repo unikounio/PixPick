e035df6de292b754143ab09dd62bbc53842fafea
unikounio <bubbbble428@gmail.com>
2025-01-10 20:49:25 +0900

    destroy_contest_if_no_participantsのモデルスペックを追加

--- spec/models/participant_spec.rb
+++ spec/models/participant_spec.rb
@@ -36,4 +36,25 @@ RSpec.describe Participant do
       end
     end
   end
+
+  describe 'callbacks' do
+    context 'when a participant is destroyed' do
+      it 'destroys the contest if no participants remain' do
+        participant = create(:participant, user:, contest:)
+
+        expect(Contest.exists?(contest.id)).to be true
+
+        participant.destroy
+
+        expect(Contest.exists?(contest.id)).to be false
+      end
+
+      it 'does not destroy the contest if participants remain' do
+        participants = create_list(:participant, 2, contest: contest)
+        participants.first.destroy
+
+        expect(Contest.exists?(contest.id)).to be true
+      end
+    end
+  end
 end
