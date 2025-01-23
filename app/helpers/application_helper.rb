178cc6513e6c1848c5e0d5670f5c39624e0dd97f
unikounio <bubbbble428@gmail.com>
2025-01-24 00:21:24 +0900

    Flashメッセージ関連のhelperメソッドをFlashHelperに移動した

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -24,23 +24,6 @@ module ApplicationHelper
     }
   end
 
-  def tailwind_classes_for(flash_type)
-    {
-      notice: 'bg-white border-l-4 border-green-500 text-black',
-      participation_notice: 'bg-white border-l-4 border-green-500 text-black',
-      alert: 'bg-white border-l-4 border-red-500 text-black',
-      participation_alert: 'bg-white border-l-4 border-red-500 text-black'
-    }.stringify_keys[flash_type.to_s] || flash_type.to_s
-  end
-
-  def notice_type?(flash_type)
-    %w[notice participation_notice].include?(flash_type.to_s)
-  end
-
-  def alert_type?(flash_type)
-    %w[alert participation_alert].include?(flash_type.to_s)
-  end
-
   def current_contest_or_contests_index_path(contest, current_user)
     if contest&.persisted?
       contest_path(contest)
--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -19,48 +19,6 @@ RSpec.describe ApplicationHelper do
     end
   end
 
-  describe '#tailwind_classes_for' do
-    it 'returns the correct Tailwind CSS classes for notice' do
-      expect(helper.tailwind_classes_for(:notice)).to eq('bg-white border-l-4 border-green-500 text-black')
-    end
-
-    it 'returns the correct Tailwind CSS classes for alert' do
-      expect(helper.tailwind_classes_for(:alert)).to eq('bg-white border-l-4 border-red-500 text-black')
-    end
-
-    it 'returns the input flash type string if it does not match predefined types' do
-      expect(helper.tailwind_classes_for(:unknown_type)).to eq('unknown_type')
-    end
-  end
-
-  describe '#notice_type?' do
-    it 'returns true for notice' do
-      expect(helper.notice_type?('notice')).to be(true)
-    end
-
-    it 'returns true for participation_notice' do
-      expect(helper.notice_type?('participation_notice')).to be(true)
-    end
-
-    it 'returns false for other types' do
-      expect(helper.notice_type?('alert')).to be(false)
-    end
-  end
-
-  describe '#alert_type?' do
-    it 'returns true for alert' do
-      expect(helper.alert_type?('alert')).to be(true)
-    end
-
-    it 'returns true for participation_alert' do
-      expect(helper.alert_type?('participation_alert')).to be(true)
-    end
-
-    it 'returns false for other types' do
-      expect(helper.alert_type?('notice')).to be(false)
-    end
-  end
-
   describe '#current_contest_or_contests_index_path' do
     let(:user) { create(:user) }
 
