9c91452768c74e8555b8d3545be7e287043498ae
unikounio <bubbbble428@gmail.com>
2025-01-23 23:51:33 +0900

    Linterの指摘に対応 Entryモデルに`Metrics/ClassLength: Class has too many lines.`と指摘を受けたので整理した。

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -1,6 +1,9 @@
 # frozen_string_literal: true
 
 class Entry < ApplicationRecord
+  include ImageResizer
+  include ImageTempfileHandler
+
   belongs_to :contest
   belongs_to :user
   has_many :votes, dependent: :destroy
@@ -24,7 +27,7 @@ class Entry < ApplicationRecord
     transaction do
       entry = create!(user: current_user, contest: contest)
 
-      resized_image_data, format, content_type = resize_and_convert_image(
+      resized_image_data, format, content_type = prepare_image_for_attach(
         file,
         file.content_type,
         400, 400
@@ -90,48 +93,16 @@ class Entry < ApplicationRecord
     errors.add(:image, '対応していない形式のファイルです')
   end
 
-  def self.resize_and_convert_image(image_data, mime_type, width, height)
+  def self.prepare_image_for_attach(image_data, mime_type, width, height)
     format = mime_type_to_format(mime_type) || 'webp'
     tempfile = create_tempfile(image_data, format)
 
     begin
-      image_processor = ImageProcessing::Vips
-                        .source(tempfile.path)
-                        .saver(strip: true)
-                        .resize_to_fit(width, height)
-
-      if %w[image/heic image/heif application/octet-stream].include?(mime_type)
-        content_type = mime_type
-      else
-        image_processor = image_processor.saver(format: 'webp', quality: 90)
-        content_type = 'image/webp'
-      end
-
-      resized_image = image_processor.call
-
-      [File.binread(resized_image.path), format, content_type]
+      resized_image_data, content_type = resize_and_convert_image(tempfile.path, mime_type, width, height)
+      [resized_image_data, format, content_type]
     ensure
       tempfile.close!
       tempfile.unlink
-    end
-  end
-
-  def self.create_tempfile(image_data, format)
-    tempfile = Tempfile.new(['image', ".#{format}"])
-    tempfile.binmode
-    tempfile.write(image_data.read)
-    tempfile.rewind
-    tempfile
-  end
-
-  def self.mime_type_to_format(mime_type)
-    case mime_type
-    when 'image/jpeg', 'image/jpg', 'image/heic', 'image/heif' then 'jpg'
-    when 'image/png'                                           then 'png'
-    when 'image/webp'                                          then 'webp'
-    else
-      Rails.logger.warn "対応していないMIMEタイプです: #{mime_type}"
-      nil
     end
   end
 
@@ -152,5 +123,5 @@ class Entry < ApplicationRecord
     ranked_entries
   end
 
-  private_class_method :resize_and_convert_image, :create_tempfile, :mime_type_to_format, :calculate_ranks
+  private_class_method :prepare_image_for_attach, :calculate_ranks
 end
